1741c25039ee0868037b6240f476306eb5eae0f5
Brittany Van Dyk <bvandyk@uwo.ca>
2020-10-28 00:32:02 +0100

    Add BMI function

--- backend/functions.py
+++ backend/functions.py
@@ -3,3 +3,11 @@ def largestNumber(num):
         return num
     else:
         return 0
+
+def calculateBMI(weight, height):
+    if weight >65 and height <165:
+        return "Overweight"
+    elif weight >25 and height >165:
+        return "Underweight"
+    else:
+        return "Healthy weight"
